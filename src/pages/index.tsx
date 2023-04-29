2d266bc92ba72d0af5dc4708ad9acafbbe79211d
wuhanjun <824786827@qq.com>
2023-04-29 10:16:13 +0800

    feat: by4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By3{' '}
+            By4{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
